ab559f71a18cb1ccecdaf7a4915468eaa0e60ffa
solaimooon <118716931+solaimooon@users.noreply.github.com>
2025-06-23 15:25:25 +0330

    prepare site for eache mosque

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import *
-app_name='website'
+
+app_name = 'website'
 urlpatterns = [
-    path('', index ,name='index'),
-    path('rez/<str:slug>/', rez_page ,name='rez_page'),
+    path('', index, name='index'),
+    path('rez/<str:slug>/', rez_page, name='rez_page'),
+    path('masje_emam_sajjad/', masjed_emama_sajad, name='masjed_emam_sajad'),
+path('masje_emam_sajjad_live/', masjed_emama_sajad_live, name='masjed_emam_sajad_live'),
+
 
 ]
